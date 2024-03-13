cbdfc5b530dc36263db96c8329df8de9c21a3403
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2024-03-13 22:49:15 +0100

    add interfaces

---
+++ src/es/uned/lsi/eped/pract2023_2024/Tune.java
@@ -0,0 +1,16 @@
+package es.uned.lsi.eped.pract2023_2024;
+
+public class Tune implements TuneIF{
+
+	public Tune(String nextToken, String nextToken2, String nextToken3, String nextToken4, Integer valueOf,
+			Integer valueOf2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean match(QueryIF q) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
