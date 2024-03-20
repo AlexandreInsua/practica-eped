2c86a05937b4bdd1367a7e943a645007c790c25e
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2024-03-20 22:30:04 +0100

    feat(project): implements tune

--- src/es/uned/lsi/eped/pract2023_2024/Query.java
+++ src/es/uned/lsi/eped/pract2023_2024/Query.java
@@ -2,17 +2,17 @@ package es.uned.lsi.eped.pract2023_2024;
 
 public class Query implements QueryIF {
 
-	String title;
-	String author;
-	String genre;
-	String album;
-	int min_year;
-	int max_year;
-	int min_duration;
-	int max_duration;
+	private String title;
+	private String author;
+	private String genre;
+	private String album;
+	private int min_year;
+	private int max_year;
+	private int min_duration;
+	private int max_duration;
 
-	public Query(String title, String author, String genre, String album, int min_year, int max_year, int min_duration,
-			int max_duration) {
+	public Query(String title, String author, String genre, String album, int min_year,
+			int max_year, int min_duration, int max_duration) {
 		super();
 		this.title = title;
 		this.author = author;
--- src/es/uned/lsi/eped/pract2023_2024/Tune.java
+++ src/es/uned/lsi/eped/pract2023_2024/Tune.java
@@ -1,16 +1,62 @@
 package es.uned.lsi.eped.pract2023_2024;
 
-public class Tune implements TuneIF{
+public class Tune implements TuneIF {
 
-	public Tune(String nextToken, String nextToken2, String nextToken3, String nextToken4, Integer valueOf,
-			Integer valueOf2) {
-		// TODO Auto-generated constructor stub
+	private String title;
+	private String author;
+	private String genre;
+	private String album;
+	private int year;
+	private int duration;
+
+	public Tune(String title, String author, String genre, String album, int year,
+			int duration) {
+		super();
+
+		if (title.equals("") || author.equals("") || genre.equals("") || album.equals("")
+				|| year <= 0 || duration <= 0)
+			throw new IllegalArgumentException();
+
+		this.title = title;
+		this.author = author;
+		this.genre = genre;
+		this.album = album;
+		this.year = year;
+		this.duration = duration;
 	}
 
 	@Override
 	public boolean match(QueryIF q) {
-		// TODO Auto-generated method stub
-		return false;
+		return matchTitle(q) && matchAuthor(q) && matchGenre(q) && matchAlbum(q)
+				&& matchYear(q) && matchDuration(q);
+	}
+
+	private boolean matchTitle(QueryIF q) {
+		return q.getTitle().equals("") || q.getTitle().equals(this.title);
+	}
+
+	private boolean matchAuthor(QueryIF q) {
+		return q.getAuthor().equals("") || q.getAuthor().equals(this.author);
+	}
+
+	private boolean matchGenre(QueryIF q) {
+		return q.getGenre().equals("") || q.getGenre().equals(this.genre);
+	}
+
+	private boolean matchAlbum(QueryIF q) {
+		return q.getAlbum().equals("") || q.getAlbum().equals(this.album);
+	}
+
+	private boolean matchYear(QueryIF q) {
+		return q.getMin_year() == -1
+				|| q.getMin_year() <= this.year && q.getMax_year() == -1
+				|| q.getMax_year() >= this.year;
+	}
+
+	private boolean matchDuration(QueryIF q) {
+		return q.getMin_duration() == -1
+				|| q.getMin_duration() <= this.duration && q.getMax_duration() == -1
+				|| q.getMax_duration() >= this.duration;
 	}
 
 }
