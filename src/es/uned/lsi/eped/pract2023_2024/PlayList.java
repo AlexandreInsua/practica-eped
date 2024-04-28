b69f5af97542e7c9a8b7ee0625e360eb2a5b0fb3
Alexandre Insua Moreira <alexandreinsua@gmail.com>
2024-04-28 17:42:40 +0200

    feat(project) add playList implementation

--- src/es/uned/lsi/eped/pract2023_2024/PlayList.java
+++ src/es/uned/lsi/eped/pract2023_2024/PlayList.java
@@ -6,31 +6,31 @@ import es.uned.lsi.eped.DataStructures.ListIF;
 
 public class PlayList implements PlayListIF {
 
-	
-	private List<Tune> tunes; 
-	
-	PlayList(String playListId){
-	
-	}
-	
-	
-	
+	private List<Integer> tuneIds;
+
 	@Override
 	public ListIF<Integer> getPlayList() {
-		// TODO Auto-generated method stub
-		return null;
+		return tuneIds;
 	}
 
 	@Override
 	public void addListOfTunes(ListIF<Integer> lT) {
-		// TODO Auto-generated method stub
-
+		IteratorIF<Integer> it = lT.iterator();
+		while (it.hasNext()) {
+			tuneIds.insert(tuneIds.size() + 1, it.getNext());
+		}
 	}
 
 	@Override
 	public void removeTune(int tuneID) {
-		// TODO Auto-generated method stub
-
+		IteratorIF<Integer> it = tuneIds.iterator();
+		int index = 0;
+		while (it.hasNext()) {
+			Integer currentTuneId = it.getNext();
+			if ( currentTuneId == tuneID) {
+				this.tuneIds.remove(index);
+			}
+			index++;
+		}
 	}
-
 }
